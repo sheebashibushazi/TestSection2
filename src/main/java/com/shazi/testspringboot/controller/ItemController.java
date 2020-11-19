44bb5ad5609b7a2018d3162efe1919af6f0570c4
sheebashibushazi <sheebashibushazi2017.com>
2020-11-19 17:47:39 +0530

    removed objectmapper added jackson and lombok

--- src/main/java/com/shazi/testspringboot/controller/ItemController.java
+++ src/main/java/com/shazi/testspringboot/controller/ItemController.java
@@ -27,7 +27,7 @@ public class ItemController {
 	//access pattern:http://localhost:9090/api/v1/getAllitems
 	@RequestMapping("/getAllitems")
 	@ResponseBody
-	public String getAllItems()
+	public List<Item> getAllItems()
 	{
 		List<Item> list1=new ArrayList<>();
 		list1=itemService.getallItems();
@@ -36,69 +36,30 @@ public class ItemController {
 		List<Item> list2= list1.stream().sorted((Item itm1,Item itm2)->(itm2.getPrice()-itm1.getPrice())).collect(Collectors.toList());		
 		list2.forEach(s->System.out.println(s));
 		 
-		// Create ObjectMapper object.
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        
-        try {
-        	String json1 = mapper.writeValueAsString(list2);
-        	System.out.println(json1);
-        	return json1;       
-        	}
-        catch (Exception e)
-        	{
-			// TODO: handle exception
-        	e.printStackTrace();
-        	return null;
-        	}
+		return list2;
 	}
 	
 //access pattern:-> http://localhost:9090/api/v1/itemId/1
 @GetMapping("/itemId/{id}")
-public String getItemById(@PathVariable int id)
+public Item getItemById(@PathVariable int id)
 {
 	ObjectMapper mapper = new ObjectMapper();
 	mapper.enable(SerializationFeature.INDENT_OUTPUT);
 	Item itm= itemService.getItemById(id);
-	try {
-		String json1 = mapper.writeValueAsString(itm);
-		System.out.println(json1);
-		return json1;       
-		}
-	catch (Exception e)
-		{
-		// TODO: handle exception
-		e.printStackTrace();
-		return null;
-		}
-
-	
+	return itm;
 
 }
 
 //access pattern:-> http://localhost:9090/api/v1/setofItemIds/1,3,4,5
 @GetMapping("/setofItemIds/{ids}")
-public String getAllids(@PathVariable List<Integer> ids )
+public List<Item>  getAllids(@PathVariable List<Integer> ids )
 {
 	/*
 	 * for(Integer i:ids) { System.out.println("id: "+i); }
 	 */	
 List<Item> itemlist=itemService.getItemsByIds(ids);
 itemlist.forEach(s->System.out.println("Item ID: "+s.getItemId()+" Description: " +s.getDescription()+" Price: "+s.getPrice()+" Brand: "+s.getBrand()));
-ObjectMapper mapper = new ObjectMapper();
-mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-try {
-	String json1 = mapper.writeValueAsString(itemlist);
-	System.out.println(json1);
-	return json1;       
-	}
-catch (Exception e)
-	{
-	// TODO: handle exception
-	e.printStackTrace();
-	return null;
-	}
+return itemlist;
 
 }
 	
--- src/main/java/com/shazi/testspringboot/model/Item.java
+++ src/main/java/com/shazi/testspringboot/model/Item.java
@@ -1,13 +1,19 @@
 package com.shazi.testspringboot.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+
 @Table(name="items")
+
 public class Item {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,47 +21,6 @@ public class Item {
 	private int price;
 	private String description;
 	private String brand;
-	
-	public Item() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Item(int itemId, int price, String description, String brand) {
-		super();
-		this.itemId = itemId;
-		this.price = price;
-		this.description = description;
-		this.brand = brand;
-	}
-	public int getItemId() {
-		return itemId;
-	}
-	public void setItemId(int itemId) {
-		this.itemId = itemId;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	/*
-	 * @Override public String toString() { return "Item [itemId=" + itemId +
-	 * ", price=" + price + ", description=" + description + ", brand=" + brand +
-	 * "]"; }
-	 */
-	
+
 
 }
