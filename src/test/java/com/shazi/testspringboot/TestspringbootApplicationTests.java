0d6efb3a6fa881b6eaaaabe0dfb75e6e6ddd2e9e
sheebashibushazi <71386661+sheebashibushazi@users.noreply.github.com>
2020-12-16 09:54:54 +0530

    Update TestspringbootApplicationTests.java

--- src/test/java/com/shazi/testspringboot/TestspringbootApplicationTests.java
+++ src/test/java/com/shazi/testspringboot/TestspringbootApplicationTests.java
@@ -2,12 +2,21 @@ package com.shazi.testspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class TestspringbootApplicationTests {
-
+@InjectMocks
+	private ItemController itemcontroller;
+	@Mock
+	private ItemService itemService;
+	
 	@Test
-	void contextLoads() {
+	public void testgetallitems() {
+		List<Item> list1=Stream.of(new Item(1, 123, "d1", "b1"), new Item(2, 234, "d2", "b2")).collect(Collectors.toList());
+		System.out.println(list1);
+		when(itemService.getallItems()).thenReturn(list1);		
+		assertEquals(2, itemService.getallItems().size());
 	}
+
 
 }
