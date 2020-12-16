b649181d7f49bce9a10c1b686749a68bc29d2c92
sheebashibushazi <71386661+sheebashibushazi@users.noreply.github.com>
2020-12-16 09:57:53 +0530

    Create ItemServiceInterface.java

---
+++ src/main/java/com/shazi/testspringboot/service/ItemServiceInterface.java
@@ -0,0 +1,11 @@
+package com.shazi.testspringboot.service;
+
+import java.util.List;
+
+import com.shazi.testspringboot.model.Item;
+
+public interface ItemServiceInterface 
+{
+	public List<Item> getallItems();
+
+}
